71b9acc857113158446fb6df168e92985bbd8a97
Дамир Шайхисламов < >
2017-03-15 00:25:37 +0500

    The count of threads in ParallelPortScanner is increased to the maximum possible.

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,24 @@ import models.Port;
 import java.net.UnknownHostException;
 import java.util.List;
 
-public class Main {
+public class Main
+{
     public static void main(String[] args) throws UnknownHostException {
         long startTime = System.currentTimeMillis();
 
         List<Port> openPorts = new ParallelPortScanner() // new ConsistentPortScanner()
-                .setHost("ntp2.stratum1.ru")
-                .setOptions(Port.TCP | Port.UDP)
-                .setStartPort(50)
-                .setFinalPort(130)
-                .setTimeout(500)
+                .setHost("hudeemvmeste.kz")
+                .setOptions(Port.TCP)
+                .setStartPort(1)
+                .setFinalPort(500)
+                .setTimeout(100)
                 .setLogMode(true)
                 .scan();
 
         System.out.println("\nOpened ports:");
         openPorts.forEach(port -> System.out.println(port.toString()));
 
-        System.out.println("Complete in: " + (System.currentTimeMillis() - startTime - 3000) / 1000 + "s.");
+        System.out.println("Complete in: " + (System.currentTimeMillis() - startTime) / 1000 + "s.");
     }
 
 }
--- src/implementations/ParallelPortScanner.java
+++ src/implementations/ParallelPortScanner.java
@@ -8,6 +8,8 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 
 public class ParallelPortScanner extends ConsistentPortScanner
 {
@@ -19,9 +21,17 @@ public class ParallelPortScanner extends ConsistentPortScanner
         for (int i = 0; i < ports.length ; i++)
             ports[i] = startPort + i;
 
-        Arrays.stream(ports)
-                .parallel()
-                .forEach(this::portIdentify);
+        ForkJoinPool forkJoinPool = new ForkJoinPool(20);
+        try {
+            forkJoinPool.submit(() -> {
+                        Arrays.stream(ports)
+                                .parallel()
+                                .unordered()
+                                .forEach(this::portIdentify);
+                    }).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
 
         return openedPorts;
     }
